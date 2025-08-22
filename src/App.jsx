761fa63ef9646873e339c273a94d98a9d03f019c
AymanChaudhary <aymanchaudhary96@gmail.com>
2025-08-22 14:34:40 +0530

    Updated route hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import HeroSection from './components/heroSection.jsx'
+import HeroSection from './components/HeroSection.jsx'
 import FeatureSection from './components/FeatureSection'
 import Workflow from './components/Workflow.jsx'
 import Pricing from './components/Pricing.jsx'
